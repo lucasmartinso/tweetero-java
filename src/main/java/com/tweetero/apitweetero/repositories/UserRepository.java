70f8a1d2818a3895bcd975348464c592bb65d081
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2023-01-24 11:43:19 -0300

    feat: create user repository

---
+++ src/main/java/com/tweetero/apitweetero/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.tweetero.apitweetero.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tweetero.apitweetero.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {}
