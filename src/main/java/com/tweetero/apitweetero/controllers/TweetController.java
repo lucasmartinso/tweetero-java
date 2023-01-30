6cfaa0dba61cc08046268e886e73705f7582c34d
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2023-01-30 17:51:29 -0300

    feat: post tweet is done

--- src/main/java/com/tweetero/apitweetero/controllers/TweetController.java
+++ src/main/java/com/tweetero/apitweetero/controllers/TweetController.java
@@ -4,19 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tweetero.apitweetero.dto.TweetDTO;
+import com.tweetero.apitweetero.model.Tweet;
+import com.tweetero.apitweetero.services.TweetService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping
 @CrossOrigin(origins = "*")
 public class TweetController {
     
-    //@Autowired
-    //private 
+    @Autowired
+    private TweetService service;
 
-    // @PostMapping("/tweet")
-    // public ResponseEntity publishTweet() {
-        
-    // }
+    @PostMapping("/tweet")
+    public ResponseEntity publishTweet(@RequestBody @Valid TweetDTO req) {
+        Tweet created = service.publish(req);
+
+        if(created == null) { 
+            ResponseEntity.status(409).build();
+        }
+
+        return ResponseEntity.ok("OK");
+    }
 }
